ccbd9fca7fdad259bd4bf5a5f9a6c131ab1259b6
nicobaldo1192 <nicobaldo_1192@hotmail.com>
2024-08-21 17:55:37 -0300

    agregando archivos al repositorio

---
+++ js/main.js
@@ -0,0 +1,72 @@
+//ok??
+//let respuesta = prompt ("como te llamas")
+//alert ("debes aceptar los terminos y condiciones")
+//let confirmar = confirm ("confirmas que aceptas los terminos y condiciones")
+
+//ok?
+//let producto = "gabinete"
+
+//if(producto == "ram"){ 
+//    console.log("agregaste un ram")
+//} else if(producto =="gabinete") { 
+//    console.log("agregaste un gabinete")
+//} else{  
+//    console.log("agregaste mouse")
+//}
+
+//INTERACCION 1 ok?
+//function saludar (){
+//    console.log("hola, bienvenido")
+//}
+
+//saludar ()
+
+// INTERACCION 2 ok?
+//function sumar(){
+//    let productoA = 60000
+//    let productoB = 90000
+//    let resultado = productoA + productoB
+
+//    return resultado
+//}
+
+//console.log(sumar())
+//let calculo = sumar()
+//console.log("La suma del valor los productos es: "+calculo)
+
+//INTERACCION 3 ok?
+
+//let productouno = parseInt(prompt("Ingrese el primer valor"))
+//let productodos = parseInt(prompt("Ingrese el segundo valor"))
+
+//function sumar(valorA, valorB){
+//    let resultado = valorA + valorB
+
+//    alert("Presupuesto "+resultado)
+//}
+
+//sumar(productouno, productodos)
+
+// FOR  ¿¿¿¿¿¿¿¿¿ como aplicarlo a la pagina ...?????????
+// ejemplo:clase2...
+//let numero = parseInt(prompt("Bienvenido! Que tabla de multiplicar desea?"))
+
+//for(let i =1; i<=15; i++){
+//    let resultado = i*numero
+//    console.log(numero+"x"+i+";"+resultado)
+//}
+
+//ARRAYS
+//const componente1 = "Placa de red"
+//const componente2 = "Placa de sonido"
+//const componente3 = "Ram"
+//const componente4 = "Placa de video"           // COMPONENTES
+//const componente5 = "Fuente de alimentacion"
+//const componente6 = "Disco duro"
+
+//const componentes = ["Placa de red", "Placa de sonido", "Ram", "Placa de video", "Fuente de alimentacion","Disco duro"]
+//console.log(componentes)
+
+//componentes.push("gabinete")
+//componentes.unshift("motherboard")
+//console.log(componentes.includes("Auriculares"))
