48504f120ef8f06c6adbb5c1109cd175e8f56872
nicobaldo1192 <nicobaldo_1192@hotmail.com>
2024-08-26 19:57:45 -0300

    re-entrega

--- js/main.js
+++ js/main.js
@@ -1,70 +1,65 @@
-//ok??
-//let respuesta = prompt ("como te llamas")
-//alert ("debes aceptar los terminos y condiciones")
-//let confirmar = confirm ("confirmas que aceptas los terminos y condiciones")
+let respuesta = prompt ("como te llamas")
+alert ("debes aceptar los terminos y condiciones")
+let confirmar = confirm ("confirmas que aceptas los terminos y condiciones")
 
-//ok?
-//let producto = "gabinete"
+let producto = "gabinete"
 
-//if(producto == "ram"){ 
-//    console.log("agregaste un ram")
-//} else if(producto =="gabinete") { 
-//    console.log("agregaste un gabinete")
-//} else{  
-//    console.log("agregaste mouse")
-//}
+if(producto == "ram"){ 
+    console.log("agregaste un ram")
+} else if(producto =="gabinete") { 
+    console.log("agregaste un gabinete")
+} else{  
+    console.log("agregaste mouse")
+}
 
-//INTERACCION 1 ok?
-//function saludar (){
-//    console.log("hola, bienvenido")
-//}
+function saludar (){
+    console.log("hola, bienvenido")
+}
 
-//saludar ()
+saludar ()
 
-// INTERACCION 2 ok?
-//function sumar(){
-//    let productoA = 60000
-//    let productoB = 90000
-//    let resultado = productoA + productoB
+function sumar(){
+    let productoA = 60000
+    let productoB = 90000
+    let resultado = productoA + productoB
 
-//    return resultado
-//}
+    return resultado
+}
 
-//console.log(sumar())
-//let calculo = sumar()
-//console.log("La suma del valor los productos es: "+calculo)
+console.log(sumar())
 
-//INTERACCION 3 ok?
+let calculo = sumar()
+console.log("La suma del valor los productos es: "+calculo)
 
-//let productouno = parseInt(prompt("Ingrese el primer valor"))
-//let productodos = parseInt(prompt("Ingrese el segundo valor"))
+let productouno = parseInt(prompt("Ingrese el primer valor"))
+let productodos = parseInt(prompt("Ingrese el segundo valor"))
 
-//function sumar(valorA, valorB){
-//    let resultado = valorA + valorB
+function sumar(valorA, valorB){
+    let resultado = valorA + valorB
 
-//    alert("Presupuesto "+resultado)
-//}
+    alert("Presupuesto "+resultado)
+}
 
-//sumar(productouno, productodos)
+sumar(productouno, productodos)
 
-//let numero = parseInt(prompt("Bienvenido! Que tabla de multiplicar desea?"))
+let numero = 0
 
-//for(let i =1; i<=15; i++){
-//    let resultado = i*numero
-//    console.log(numero+"x"+i+";"+resultado)
-//}
+while (numero = 10){
+    numero+
+    alert (numero)
+    break
+}
 
-//ARRAYS
-//const componente1 = "Placa de red"
-//const componente2 = "Placa de sonido"
-//const componente3 = "Ram"
-//const componente4 = "Placa de video"           // COMPONENTES
-//const componente5 = "Fuente de alimentacion"
-//const componente6 = "Disco duro"
+const componente1 = "Placa de red"
+const componente2 = "Placa de sonido"
+const componente3 = "Ram"
+const componente4 = "Placa de video"           
+const componente5 = "Fuente de alimentacion"
+const componente6 = "Disco duro"
 
-//const componentes = ["Placa de red", "Placa de sonido", "Ram", "Placa de video", "Fuente de alimentacion","Disco duro"]
-//console.log(componentes)
+const componentes = ["Placa de red", "Placa de sonido", "Ram", "Placa de video", "Fuente de alimentacion","Disco duro"]
+console.log(componentes)
 
-//componentes.push("gabinete")
-//componentes.unshift("motherboard")
-//console.log(componentes.includes("Auriculares"))
+componentes.push("gabinete")
+componentes.unshift("motherboard")
+console.log(componentes.includes("Auriculares"))
